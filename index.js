e7217945b49a7e3f490a74dc3bd9c14e297e4d96
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-10 20:45:58 -0500

    Add append method to linkedList

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const Node = (newValue = null) => {
+const node = (newValue = null, next = null) => {
   let value = newValue;
+  let nextNode = next;
   const getValue = () => value;
+  const getNextNode = () => nextNode;
+  const setNextNode = (n) => {
+    nextNode = n;
+  };
 
-  return { getValue };
+  return { getValue, getNextNode, setNextNode };
 };
 
-const LinkedList = () => {
+const linkedList = () => {
   let head = null;
-  const getHead = () => head;
 
-  return { getHead };
+  const append = (value) => {
+    const newNode = node(value);
+
+    if (head) {
+      let currentNode = head;
+
+      while (currentNode.getNextNode()) {
+        currentNode = currentNode.getNextNode();
+      }
+
+      currentNode.setNextNode = newNode;
+    } else {
+      head = newNode;
+    }
+  };
+
+  return { append };
 };
