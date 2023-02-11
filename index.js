54452d812fb57f61df21f8b7d8a2191d15ae50fc
Jesse Nguyen <nguyenjessev@gmail.com>
2023-02-10 20:38:51 -0500

    Add Node factory function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const Node = () => {
+  let value = null;
+  const getValue = () => value;
+
+  return { getValue };
+};
+
 const LinkedList = () => {
   let head = null;
   const getHead = () => head;
